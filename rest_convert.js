4cf31a16e4f2dbb48900f9ff4b74251276381648
not-matthias <26800596+not-matthias@users.noreply.github.com>
2018-10-18 22:45:46 +0200

    renamed a variable

--- rest_convert.js
+++ rest_convert.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch');
 const libxmljs = require('libxmljs');
 
 fetch(`http://api.wolframalpha.com/v2/query?input=${process.argv[2]}%20${process.argv[3]}%20${process.argv[4]}%20${process.argv[5]}&appid=T986V5-VU5TLV89QV`)
-    .then(res => res.text())
+    .then(response => response.text())
     .then(body => {
         if(libxmljs.parseXml(body).get('*[@primary]').text().trim().includes('not compatible.')) throw Error();
         console.log(`${process.argv[2]} ${process.argv[3]} are ${libxmljs.parseXml(body).get('*[@primary]').text().trim().split(' ') [0]} ${process.argv[5]}`);
