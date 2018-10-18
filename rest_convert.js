ca25c620aba5cd829c2e1a874c5b7079a49c056f
not-matthias <26800596+not-matthias@users.noreply.github.com>
2018-10-18 22:42:03 +0200

    added check if units are compatible

--- rest_convert.js
+++ rest_convert.js
@@ -3,5 +3,8 @@ const libxmljs = require('libxmljs');
 
 fetch(`http://api.wolframalpha.com/v2/query?input=${process.argv[2]}%20${process.argv[3]}%20${process.argv[4]}%20${process.argv[5]}&appid=T986V5-VU5TLV89QV`)
     .then(res => res.text())
-    .then(body => console.log(`${process.argv[2]} ${process.argv[3]} are ${libxmljs.parseXml(body).get('*[@primary]').text().trim().split(' ') [0]} ${process.argv[5]}`))
+    .then(body => {
+        if(libxmljs.parseXml(body).get('*[@primary]').text().trim().includes('not compatible.')) throw Error();
+        console.log(`${process.argv[2]} ${process.argv[3]} are ${libxmljs.parseXml(body).get('*[@primary]').text().trim().split(' ') [0]} ${process.argv[5]}`);
+    })
     .catch(() => console.log('Invalid parameters'));
