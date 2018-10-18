14bfa79373378ae6823fa81a412a4a50eb41d01c
not-matthias <26800596+not-matthias@users.noreply.github.com>
2018-10-18 21:10:38 +0200

    first solution, changed gitignore, added smartcar-unit for later testing

--- convert.js
+++ convert.js
@@ -1,1 +1,11 @@
-console.log('Not implemented yet!');
+const conversions = {
+    "m": 1,
+    "cm": .01,
+    "mm": .001,
+    "kg": 1,
+    "g": .001,
+}
+if (!isNaN(process.argv[2]) && conversions[process.argv[3]] && process.argv[4] === "to" && conversions[process.argv[5]])
+    console.log(`${process.argv[2]} ${process.argv[3]} are ${process.argv[2] * conversions[process.argv[3]] / conversions[process.argv[5]]} ${process.argv[5]}`)
+else
+    console.log("Invalid parameters");
