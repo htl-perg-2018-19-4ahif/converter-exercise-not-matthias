edb645c6fc3f4d4b5ba63ce4f531900930914227
not-matthias <26800596+not-matthias@users.noreply.github.com>
2018-10-18 21:23:23 +0200

    added eslint

--- convert.js
+++ convert.js
@@ -1,11 +1,11 @@
 const conversions = {
-    "m": 1,
-    "cm": .01,
-    "kg": 1,
-    "g": .001,
-}
+    'm': 1,
+    'cm': .01,
+    'kg': 1,
+    'g': .001,
+};
 
-if (!isNaN(process.argv[2]) && conversions[process.argv[3]] && process.argv[4] === "to" && conversions[process.argv[5]])
-    console.log(`${process.argv[2]} ${process.argv[3]} are ${process.argv[2] * conversions[process.argv[3]] / conversions[process.argv[5]]} ${process.argv[5]}`)
+if (!isNaN(process.argv[2]) && conversions[process.argv[3]] && process.argv[4] === 'to' && conversions[process.argv[5]])
+    console.log(`${process.argv[2]} ${process.argv[3]} are ${process.argv[2] * conversions[process.argv[3]] / conversions[process.argv[5]]} ${process.argv[5]}`);
 else
-    console.log("Invalid parameters");
+    console.log('Invalid parameters');
