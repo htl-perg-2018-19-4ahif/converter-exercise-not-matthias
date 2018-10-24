28881ad3dea5d8305d192b5011bfa90ad391ad66
Matthias <matthias.heiden.mh@gmail.com>
2018-10-24 15:18:20 +0200

    added size and "to" checks to rest convert

--- src/rest-convert.js
+++ src/rest-convert.js
@@ -5,6 +5,6 @@ fetch(`http://api.wolframalpha.com/v2/query?input=${process.argv[2]}%20${process
     .then(response => response.text())
     .then(body => {
         if(libxmljs.parseXml(body).get('*[@primary]').text().trim().includes('not compatible.')) throw Error();
-        console.log(`${process.argv[2]} ${process.argv[3]} are ${libxmljs.parseXml(body).get('*[@primary]').text().trim().split(' ') [0]} ${process.argv[5]}`);
+        console.log((process.argv[4] !== "to" || process.argv.length != 6) ? 'Invalid parameters' : `${process.argv[2]} ${process.argv[3]} are ${libxmljs.parseXml(body).get('*[@primary]').text().trim().split(' ') [0]} ${process.argv[5]}`);
     })
     .catch(() => console.log('Invalid parameters'));
