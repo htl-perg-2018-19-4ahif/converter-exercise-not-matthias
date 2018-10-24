b4b360fcf14df393442474e526679f1e699cef89
Matthias <matthias.heiden.mh@gmail.com>
2018-10-24 15:16:08 +0200

    added arguments and "to" checks

--- src/convert.js
+++ src/convert.js
@@ -1,7 +1,7 @@
 const unit = require('smartcar-unit');
 
 try {
-    console.log(`${process.argv[2]} ${process.argv[3]} are ${unit(process.argv[2], process.argv[3]).as(process.argv[5])} ${process.argv[5]}`);
+    console.log((process.argv[4] !== "to" || process.argv.length != 6) ? 'Invalid parameters' : `${process.argv[2]} ${process.argv[3]} are ${unit(process.argv[2], process.argv[3]).as(process.argv[5])} ${process.argv[5]}`);
 } catch (e) {
     console.log('Invalid parameters');
 }
